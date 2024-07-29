8eb9480b1c3dc2fb43aba3469719b7a07b4a26c6
Daniel Owen van Dommelen <theapemachine@gmail.com>
2024-07-30 00:41:06 +0200

    blocksuite

--- src/components/BlockSuiteEditor/index.tsx
+++ src/components/BlockSuiteEditor/index.tsx
@@ -1,44 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
-import '@blocksuite/presets/themes/affine.css';
-import { createEmptyDoc, PageEditor, EdgelessEditor } from '@blocksuite/presets';
-import { Text } from '@blocksuite/store';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React from "react"
+import "@blocksuite/presets/themes/affine.css"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { EditorProvider } from "./provider"
+import EditorContainer from "./container"
 
 const BlockSuiteEditor: React.FC = () => {
-    const [activeTab, setActiveTab] = useState<any>("editorTab")
-    const editorRef = useRef<HTMLDivElement | null>(null);
-    const edgelessRef = useRef<HTMLDivElement | null>(null);
-    const [initialized, setInitialized] = useState(false);
-
-    useEffect(() => {
-        if (!initialized) {
-            const doc = createEmptyDoc().init();
-            const editor = new PageEditor();
-            const edgeless = new EdgelessEditor();
-
-            editor.doc = doc;
-            edgeless.doc = doc;
-
-            if (editorRef.current) editorRef.current.appendChild(editor);
-            if (edgelessRef.current) edgelessRef.current.appendChild(edgeless);
-
-            // Update block node with some initial text content
-            const paragraphs = doc.getBlockByFlavour('affine:paragraph');
-            if (paragraphs.length > 0) {
-                const paragraph = paragraphs[0];
-                doc.updateBlock(paragraph, { text: new Text('Hello World!') });
-            }
-
-            setInitialized(true);
-        }
-    }, [initialized]);
-
     return (
-        <Tabs 
-            defaultValue="editorTab" 
-            onValueChange={(value: any) => {
-                setActiveTab(value)
-            }} 
+        <Tabs
+            defaultValue="editorTab"
             className="w-full"
         >
             <TabsList className="grid w-full grid-cols-2">
@@ -46,22 +15,15 @@ const BlockSuiteEditor: React.FC = () => {
                 <TabsTrigger value="edgelessTab">Edgeless</TabsTrigger>
             </TabsList>
             <TabsContent value="editorTab">
-                <div ref={editorRef} style={{ 
-                    width: "100%", 
-                    height: "100vh",
-                    display: activeTab === "editorTab" ? "block" : "none"
-                }}></div>
+                <EditorProvider>
+                    <EditorContainer />
+                </EditorProvider>
             </TabsContent>
             <TabsContent value="edgelessTab">
-                <div ref={edgelessRef} style={{ 
-                    width: "100%", 
-                    height: "100vh",
-                    display: activeTab === "edgelessTab" ? "block" : "none"
-                }}></div>
             </TabsContent>
         </Tabs>
-    );
+    )
 }
 
-export default BlockSuiteEditor;
+export default BlockSuiteEditor
 
